b4806b6199057355e790cfd91e952b4e9bc6c6de
Jan Cerny <honzaa.cerny@gmail.com>
2022-11-29 14:59:07 +0100

    Create ZnovaPytn.py

---
+++ ZnovaPytn.py
@@ -0,0 +1,13 @@
+cisla = []
+cislo = "cokoliv"
+
+#začnu od konce, tj. prázdno
+while cislo:
+      cislo = input("Zadej číslo: ")
+      if cislo != " ":
+            cisla.append(int(cislo))
+cisla_2 = sorted(cisla)
+# zjednodušený medián
+median = cisla_2[len(cisla_2)//2]
+for cislo in cisla:
+    print(cislo, "se od mediánu odlišuje o", cislo - median)
